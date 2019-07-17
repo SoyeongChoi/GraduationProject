a20011c4dfd28ee158a2581e41297d1e5d0625d8
Kyu Young Sim <tlarbdud0703@naver.com>
2019-07-17 13:23:50 +0900

    [data_generator] DataGenerator 클래스 추가
    클래스 추가 및 클래스에 맞도록 코드 수정중.
    (AI Hub data를 기다리며, AffectNet에 맞게 작성하던 코딩 작업 일시 정지 상태)

--- Data_Loader/data_generator.py
+++ Data_Loader/data_generator.py
@@ -5,96 +5,116 @@ import pandas as pd
 
 
 
-# class DataGenerator(self, selection = [0,1,2,3,6], batch_):
+class DataGenerator:
+
+    # Data
+    def __init__(self, labels_output_format=('class_id', 'xmin', 'ymin', 'xmax', 'ymax'))
+
+        '''
+        :param labels_output_format (list, optional): A list of five strings representing the desired order of the five
+                items class ID, xmin, ymin, xmax, ymax in the generated ground truth data (if any). The expected
+                strings are 'xmin', 'ymin', 'xmax', 'ymax', 'class_id'.
+        '''
+        self.labels_output_format = labels_output_format
+        self.labels_format = {'class_id': labels_output_format.index('class_id'),
+                              'xmin': labels_output_format.index('xmin'),
+                              'ymin': labels_output_format.index('ymin'),
+                              'xmax': labels_output_format.index('xmax'),
+                              'ymax': labels_output_format.index('ymax')}  # This dictionary is for internal use.
+
+
+        self.data_path = os.path.join('../', 'data', 'Manually_Annotated')
+
+        self.expression_list = ['Neutral', 'Happy', 'Sad', 'Surprise', 'Fear', 'Disgust', 'Anger', 'Contempt', 'None', 'Uncertain',
+                       ' Non-face']
+        self.expression_size = [75374, 134915, 25959, 14590, 6878, 4303, 25382, 4250, 33588, 12145, 82915]
+        ## netural, happy, sad, surprise, anger
+        ## 0, 1, 2, 3, 6 [label min size = 14590 (Surprise)/ label max size = 134915(Happy)]
+
+    def init_selection(self, selection = None):
+        self.selection
 # We Using AffectNet DataSet
-data_path = os.path.join('../', 'data', 'Manually_Annotated')
-
-# Expression List
-expression_list = ['Neutral', 'Happy', 'Sad', 'Surprise', 'Fear', 'Disgust', 'Anger', 'Contempt', 'None', 'Uncertain',
-                   ' Non-face']
-
-# Manually Annotated Expression Size
-expression_size = [75374, 134915, 25959, 14590, 6878, 4303, 25382, 4250, 33588, 12145, 82915]
-## netural, happy, sad, surprise, anger
-## 0, 1, 2, 3, 6 [label min size = 14590 (Surprise)/ label max size = 134915(Happy)]
-
-# emotion select
-selection = [0,1,2,3,6]
-
-# get selections min for same size data(label)
-selection_size_list = [expression_size[x] for x in selection]
-min_size = min(selection_size_list)
-
-##
-trainset = pd.read_csv(os.path.join(data_path, "training.csv"))
-
-colnames = ['subDirectory_filePath', 'face_x', 'face_y', 'face_width', 'face_height', 'facial_landmarks',
-            'expression', 'valence', 'arousal' ]
-
-testset = pd.read_csv(os.path.join(path, "validation.csv"), names=colnames)
 
 
-# remove 1. 'facial_landmarks' 2. 'valence' 3. 'arousal'
-# trainset.drop(['facial_landmarks', 'valence', 'arousal'],axis =1)
-# testset.drop(['facial_landmarks', 'valence', 'arousal'],axis =1)
-
-# only selected emotions remain
-trainset = dataset[(dataset['expression'].isin(selection))]
-testset = testset[(dataset['expression'].isin(selection))]
 
 
-trainset_index = dataset.reset_index().columns[0, 'expression']
-testset_index = testset.reset_index().columns[0, 'expression']
+    # emotion select
+    # selection = [0,1,2,3,6]
 
-# n * m min_size
-train_data = np.zeros(len(selection) * min_size)
-test_data = (testset_index.columns[0]).values
+    # get selections min for same size data(label)
+    selection_size_list = [expression_size[x] for x in selection]
+    min_size = min(selection_size_list)
 
+    ##
+    trainset = pd.read_csv(os.path.join(data_path, "training.csv"))
 
-# random sampling for train
-for i in selection:
-    # 이게 random sampling index
-    train_data[i*min_size] = ((dataset_index[dataset_index['expression'] == i].sample(n= min_size, random_stata=1)).columns[0]).values
+    colnames = ['subDirectory_filePath', 'face_x', 'face_y', 'face_width', 'face_height', 'facial_landmarks',
+                'expression', 'valence', 'arousal' ]
 
-
-print(dataset_index)
-# remove
-
-# Todo : 1. minsize로 데이터를 자르고 ,
-
-'''
-basewidth = 300
-
-for i in range(4):  # range(dataset.shape[0]):
-
-    image_path = os.path.join(data_path, dataset['subDirectory_filePath'][i])
-
-    if os.path.exists(image_path):
-        img = Image.open(image_path)
-
-        fig, ax = plt.subplots(1)
-
-        origin_size = img.size
-        wpercent = (basewidth / float(img.size[0]))
-        hsize = int((float(img.size[1]) * float(wpercent)))
-
-        img = img.resize((basewidth, hsize), Image.ANTIALIAS)
-
-        imgplot = plt.imshow(img)
-
-        rect = matplotlib.patches.Rectangle([dataset['face_x'][i] * wpercent, dataset['face_y'][i] * wpercent],
-                                            dataset['face_width'][i] * wpercent, dataset['face_height'][i] * wpercent,
-                                            linewidth=1, edgecolor='r', facecolor='none')
-        ax.add_patch(rect)
-
-        plt.show()
-
-        print("original size is ", origin_size, " => modified size is ", img.size)
-        print(i, "th data's expression is : ", expression_list[dataset['expression'][i]])
-    else:
-        print(i, 'th data is not exist')
-
-    time.sleep(1)
+    testset = pd.read_csv(os.path.join(path, "validation.csv"), names=colnames)
 
 
-'''
+    # remove 1. 'facial_landmarks' 2. 'valence' 3. 'arousal'
+    # trainset.drop(['facial_landmarks', 'valence', 'arousal'],axis =1)
+    # testset.drop(['facial_landmarks', 'valence', 'arousal'],axis =1)
+
+    # only selected emotions remain
+    trainset = dataset[(dataset['expression'].isin(selection))]
+    testset = testset[(dataset['expression'].isin(selection))]
+
+
+    trainset_index = dataset.reset_index().columns[0, 'expression']
+    testset_index = testset.reset_index().columns[0, 'expression']
+
+    # n * m min_size
+    train_data = np.zeros(len(selection) * min_size)
+    test_data = (testset_index.columns[0]).values
+
+
+    # random sampling for train
+    for i in selection:
+        # 이게 random sampling index
+        train_data[i*min_size] = ((dataset_index[dataset_index['expression'] == i].sample(n= min_size, random_stata=1)).columns[0]).values
+
+
+    print(dataset_index)
+    # remove
+
+    # Todo : 1. minsize로 데이터를 자르고 ,
+
+    '''
+    basewidth = 300
+    
+    for i in range(4):  # range(dataset.shape[0]):
+    
+        image_path = os.path.join(data_path, dataset['subDirectory_filePath'][i])
+    
+        if os.path.exists(image_path):
+            img = Image.open(image_path)
+    
+            fig, ax = plt.subplots(1)
+    
+            origin_size = img.size
+            wpercent = (basewidth / float(img.size[0]))
+            hsize = int((float(img.size[1]) * float(wpercent)))
+    
+            img = img.resize((basewidth, hsize), Image.ANTIALIAS)
+    
+            imgplot = plt.imshow(img)
+    
+            rect = matplotlib.patches.Rectangle([dataset['face_x'][i] * wpercent, dataset['face_y'][i] * wpercent],
+                                                dataset['face_width'][i] * wpercent, dataset['face_height'][i] * wpercent,
+                                                linewidth=1, edgecolor='r', facecolor='none')
+            ax.add_patch(rect)
+    
+            plt.show()
+    
+            print("original size is ", origin_size, " => modified size is ", img.size)
+            print(i, "th data's expression is : ", expression_list[dataset['expression'][i]])
+        else:
+            print(i, 'th data is not exist')
+    
+        time.sleep(1)
+    
+    
+    '''
